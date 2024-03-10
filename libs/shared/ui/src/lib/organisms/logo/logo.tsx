53ac18749a17c2b2ec1792480ef11d71ba1d212d
Gustavo Cabezal <meisterveda@gmail.com>
2024-03-10 00:57:06 +0000

    feat: added logo

---
+++ libs/shared/ui/src/lib/organisms/logo/logo.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable-next-line */
+export interface LogoProps {}
+
+export function Logo(props: LogoProps) {
+  return (
+    <div>
+      <span className="self-center ml-2 text-2xl font-extrabold text-gray-900 whitespace-nowrap dark:text-white">
+        Gustavo Cabezal
+      </span>
+    </div>
+  );
+}
+
+export default Logo;
